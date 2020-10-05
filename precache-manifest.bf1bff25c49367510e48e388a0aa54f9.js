22343be1d419c1539f8f8e2b083f26e29d5115f9
ozzbabym <62439487+ozzbabym@users.noreply.github.com>
2020-10-05 19:38:26 +0300

    Updates

---
+++ precache-manifest.bf1bff25c49367510e48e388a0aa54f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a6e34a9f730ddf5640f930a3a3d56b",
+    "url": "/demo-socialset/index.html"
+  },
+  {
+    "revision": "995993fd4437860cc9b0",
+    "url": "/demo-socialset/static/css/3.c90f66ef.chunk.css"
+  },
+  {
+    "revision": "420e4fc43656ccbf68d6",
+    "url": "/demo-socialset/static/css/4.19f601cd.chunk.css"
+  },
+  {
+    "revision": "b865780d7143972efc7b",
+    "url": "/demo-socialset/static/css/main.c58e0852.chunk.css"
+  },
+  {
+    "revision": "70c6c2f99ccad8abe0e0",
+    "url": "/demo-socialset/static/js/2.c5504822.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo-socialset/static/js/2.c5504822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995993fd4437860cc9b0",
+    "url": "/demo-socialset/static/js/3.d6b6b17f.chunk.js"
+  },
+  {
+    "revision": "420e4fc43656ccbf68d6",
+    "url": "/demo-socialset/static/js/4.2557fa31.chunk.js"
+  },
+  {
+    "revision": "b865780d7143972efc7b",
+    "url": "/demo-socialset/static/js/main.990f21d0.chunk.js"
+  },
+  {
+    "revision": "ffe03c36ec4156f18e38",
+    "url": "/demo-socialset/static/js/runtime-main.5e252e20.js"
+  },
+  {
+    "revision": "b65e3c1d6ace6db943078f8d21ffa8c0",
+    "url": "/demo-socialset/static/media/810.b65e3c1d.gif"
+  },
+  {
+    "revision": "b2fce85a27f7cd1ddd9998e8283de967",
+    "url": "/demo-socialset/static/media/user.b2fce85a.png"
+  }
+]);
